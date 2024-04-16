28e776982650ed4b045ae1de5b53e46170824f78
unknown <vismodeb2000@gmail.com>
2024-04-16 16:48:06 +0600

    Receipt section created

---
+++ src/components/home/receiptSection/ReceiptSection.jsx
@@ -0,0 +1,133 @@
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+
+const ReceiptSection = () => {
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table border border-black">
+          {/* head */}
+          <thead className="text-lg font-bold bg-[#f40027] text-white">
+            <tr>
+              <th className="border-r border-black">SL</th>
+              <th className="border-r border-black">Description</th>
+              <th className="border-r border-black">Price</th>
+              <th className="border-r border-black">QTY</th>
+              <th className="border-r border-black">Sub-total</th>
+            </tr>
+          </thead>
+          <tbody className="text-base">
+            {/* row 1 */}
+            <tr>
+              <th className="border-r border-black">01</th>
+              <td className="border-r border-black">Chicken Meat</td>
+              <td className="border-r border-black">99 TK</td>
+              <td className="flex items-center gap-2 border-r border-black">
+                <FiMinusCircle className="cursor-pointer hover:text-red-600" />1
+                <FiPlusCircle className="cursor-pointer  hover:text-red-600" />
+              </td>
+              <td>599 TK</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th className="border-r border-black">02</th>
+              <td className="border-r border-black">Meat</td>
+              <td className="border-r border-black">200 TK</td>
+              <td className="flex items-center gap-2 border-r border-black">
+                <FiMinusCircle className="cursor-pointer hover:text-red-600" />1
+                <FiPlusCircle className="cursor-pointer  hover:text-red-600" />
+              </td>
+              <td className="border-r border-black">200 Tk</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+            <tr>
+              <th className="border-r border-black"></th>
+              <td className="border-r border-black"></td>
+              <td className="border-r border-black"></td>
+              <td className="flex items-center gap-2 border-r border-black"></td>
+              <td className="border-r border-black"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ReceiptSection;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import Header from "../../components/home/header/Header";
+import ReceiptSection from "../../components/home/receiptSection/ReceiptSection";
 
 const Home = () => {
   return (
     <div className="container mx-auto">
       <Header />
-      <h1 className="text-2xl text-red-600">Home Page</h1>
+      <ReceiptSection />
     </div>
   );
 };
